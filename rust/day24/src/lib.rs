809f1cad5dcd5fdb1597cc9f9b95474d1cb22938
Daniel Kales <11509575+dkales@users.noreply.github.com>
2023-12-25 15:53:58 +0100

    feat: day24p2

--- rust/day24/src/lib.rs
+++ rust/day24/src/lib.rs
@@ -102,7 +102,7 @@ fn solve_stage1(input: &[Hailstone], limits: (i64, i64)) -> u64 {
     count
 }
 
-fn solve_stage2(input: &[Hailstone]) -> u64 {
+fn solve_stage2(input: &[Hailstone]) -> i64 {
     let x1 = input[0].pos.0 as f64;
     let y1 = input[0].pos.1 as f64;
     let z1 = input[0].pos.2 as f64;
@@ -133,85 +133,94 @@ fn solve_stage2(input: &[Hailstone]) -> u64 {
     // eq9: z0 + vz0 * t3 = z3 + vz3 * t3
 
     // eliminate t1,t2,t3 by combining eq1 & eq2, ...
-    // eq1': eq1 & eq2:     x0*vy1-x0*vy0-x1*vy1+x1vy0 = y0*vx1-v0*vx0-y1*vx1+y1*vx0
-    // eq2': eq1 & eq3:     x0*vz1-x0*vz0-x1*vz1+x1vz0 = z0*vx1-v0*vx0-z1*vx1+z1*vx0
-    // eq3': eq4 & eq5:     x0*vy2-x0*vy0-x2*vy2+x2vy0 = y0*vx2-v0*vx0-y2*vx2+y2*vx0
-    // eq4': eq4 & eq6:     x0*vz2-x0*vz0-x2*vz2+x2vz0 = z0*vx2-v0*vx0-z2*vx2+z2*vx0
-    // eq5': eq7 & eq8:     x0*vy3-x0*vy0-x3*vy3+x3vy0 = y0*vx3-v0*vx0-y3*vx3+y3*vx0
-    // eq6': eq7 & eq9:     x0*vz3-x0*vz0-x3*vz3+x3vz0 = z0*vx3-v0*vx0-z3*vx3+z3*vx0
+    // eq1': eq1 & eq2:     x0*vy1-x0*vy0-x1*vy1+x1vy0 = y0*vx1-y0*vx0-y1*vx1+y1*vx0
+    // eq2': eq1 & eq3:     x0*vz1-x0*vz0-x1*vz1+x1vz0 = z0*vx1-z0*vx0-z1*vx1+z1*vx0
+    // eq3': eq4 & eq5:     x0*vy2-x0*vy0-x2*vy2+x2vy0 = y0*vx2-y0*vx0-y2*vx2+y2*vx0
+    // eq4': eq4 & eq6:     x0*vz2-x0*vz0-x2*vz2+x2vz0 = z0*vx2-z0*vx0-z2*vx2+z2*vx0
+    // eq5': eq7 & eq8:     x0*vy3-x0*vy0-x3*vy3+x3vy0 = y0*vx3-y0*vx0-y3*vx3+y3*vx0
+    // eq6': eq7 & eq9:     x0*vz3-x0*vz0-x3*vz3+x3vz0 = z0*vx3-z0*vx0-z3*vx3+z3*vx0
+    // eq7': eq2 & eq3:     y0*vz1-y0*vz0-y1*vz1+y1vz0 = z0*vy1-z0*vy0-z1*vy1+z1*vy0
+    // eq8': eq6 & eq6:     y0*vz2-y0*vz0-y2*vz2+y2vz0 = z0*vy2-z0*vy0-z2*vy2+z2*vy0
+    // eq9': eq8 & eq9:     y0*vz3-y0*vz0-y3*vz3+y3vz0 = z0*vy3-z0*vy0-z3*vy3+z3*vy0
 
     // now these equations have the same non-linear unknowns, so we can subtract them to get rid of them
     // eq1'': eq1' - eq3':  x0 * (vy1-vy2) - x1* (vy1-vy0) - x2 * (vy0-vy2) = y0 * (vx1-vx2) - y1 * (vx1-vx0) - y2 * (vx0-vx2)
     // eq2'': eq1' - eq5':  x0 * (vy1-vy3) - x1* (vy1-vy0) - x3 * (vy0-vy3) = y0 * (vx1-vx3) - y1 * (vx1-vx0) - y3 * (vx0-vx3)
-    // eq3'': eq3' - eq5':  x0 * (vy2-vy3) - x2* (vy2-vy0) - x3 * (vy0-vy3) = y0 * (vx2-vx3) - y2 * (vx2-vx0) - y3 * (vx0-vx3)
-    // eq4'': eq2' - eq4':  x0 * (vz1-vz2) - x1* (vz1-vz0) - x2 * (vz0-vz2) = z0 * (vx1-vx2) - z1 * (vx1-vx0) - z2 * (vx0-vx2)
-    // eq5'': eq2' - eq6':  x0 * (vz1-vz3) - x1* (vz1-vz0) - x3 * (vz0-vz3) = z0 * (vx1-vx3) - z1 * (vx1-vx0) - z3 * (vx0-vx3)
-    // eq6'': eq4' - eq6':  x0 * (vz2-vz3) - x2* (vz2-vz0) - x3 * (vz0-vz3) = z0 * (vx2-vx3) - z2 * (vx2-vx0) - z3 * (vx0-vx3)
+    // eq3'': eq2' - eq4':  x0 * (vz1-vz2) - x1* (vz1-vz0) - x2 * (vz0-vz2) = z0 * (vx1-vx2) - z1 * (vx1-vx0) - z2 * (vx0-vx2)
+    // eq4'': eq2' - eq6':  x0 * (vz1-vz3) - x1* (vz1-vz0) - x3 * (vz0-vz3) = z0 * (vx1-vx3) - z1 * (vx1-vx0) - z3 * (vx0-vx3)
+    // eq5'': eq7' - eq8':  y0 * (vz1-vz2) - y1* (vz1-vz0) - y2 * (vz0-vz2) = z0 * (vy1-vy2) - z1 * (vy1-vy0) - z2 * (vy0-vy2)
+    // eq6'': eq7' - eq9':  y0 * (vz1-vz3) - y1* (vz1-vz0) - y3 * (vz0-vz3) = z0 * (vy1-vy3) - z1 * (vy1-vy0) - z3 * (vy0-vy3)
 
     // make them a bit nicer, factoring out the variables
-    // eq1'': x0 * (vy1-vy2) + vy0 * (x1-x2) - y0 * (vx1-vx2) + vx0 * (y2-y1) = -y1*vx1 + y2*vx2 + x1*vy1 - x2*vy2
-    // eq2'': x0 * (vy1-vy3) + vy0 * (x1-x3) - y0 * (vx1-vx3) + vx0 * (y3-y1) = -y1*vx1 + y3*vx3 + x1*vy1 - x3*vy3
-    // eq3'': x0 * (vy2-vy3) + vy0 * (x2-x3) - y0 * (vx2-vx3) + vx0 * (y3-y2) = -y2*vx2 + y3*vx3 + x2*vy2 - x3*vy3
-    // eq4'': x0 * (vz1-vz2) + vz0 * (x1-x2) - z0 * (vx1-vx2) + vx0 * (z2-z1) = -z1*vx1 + z2*vx2 + x1*vz1 - x2*vz2
-    // eq5'': x0 * (vz1-vz3) + vz0 * (x1-x3) - z0 * (vx1-vx3) + vx0 * (z3-z1) = -z1*vx1 + z3*vx3 + x1*vz1 - x3*vz3
-    // eq6'': x0 * (vz2-vz3) + vz0 * (x2-x3) - z0 * (vx2-vx3) + vx0 * (z3-z2) = -z2*vx2 + z3*vx3 + x2*vz2 - x3*vz3
+    // eq1'': x0 * (vy1-vy2) + y0 * (vx2-vx1) + vx0 * (y2-y1) + vy0 * (x1-x2) = -y1*vx1 + y2*vx2 + x1*vy1 - x2*vy2
+    // eq2'': x0 * (vy1-vy3) + y0 * (vx3-vx1) + vx0 * (y3-y1) + vy0 * (x1-x3) = -y1*vx1 + y3*vx3 + x1*vy1 - x3*vy3
+    // eq3'': x0 * (vz1-vz2) + z0 * (vx2-vx1) + vx0 * (z2-z1) + vz0 * (x1-x2) = -z1*vx1 + z2*vx2 + x1*vz1 - x2*vz2
+    // eq4'': x0 * (vz1-vz3) + z0 * (vx3-vx1) + vx0 * (z3-z1) + vz0 * (x1-x3) = -z1*vx1 + z3*vx3 + x1*vz1 - x3*vz3
+    // eq5'': y0 * (vz1-vz2) + z0 * (vy2-vy1) + vy0 * (z2-z1) + vz0 * (y1-y2) = -z1*vy1 + z2*vy2 + y1*vz1 - y2*vz2
+    // eq6'': y0 * (vz1-vz3) + z0 * (vy3-vy1) + vy0 * (z3-z1) + vz0 * (y1-y3) = -z1*vy1 + z3*vy3 + y1*vz1 - y3*vz3
 
     // unkown vector [x0,y0,z0, vx0,vy0,vz0]
     // build a matrix
     let mut matrix: [[f64; 7]; 6] = [
-        [
-            vz1 - vz3,
-            0.0,
-            vx1 - vx3,
-            z3 - z1,
-            0.0,
-            x1 - x3,
-            -z1 * vx1 + z3 * vx3 + x1 * vz1 - x3 * vz3,
-        ],
+        // eq1'': x0 * (vy1-vy2) + y0 * (vx2-vx1) + vx0 * (y2-y1) + vy0 * (x1-x2) = -y1*vx1 + y2*vx2 + x1*vy1 - x2*vy2
         [
             vy1 - vy2,
-            vx1 - vx2,
+            vx2 - vx1,
             0.0,
             y2 - y1,
             x1 - x2,
             0.0,
             -y1 * vx1 + y2 * vx2 + x1 * vy1 - x2 * vy2,
         ],
-        [
-            vz1 - vz2,
-            0.0,
-            vx1 - vx2,
-            z2 - z1,
-            0.0,
-            x1 - x2,
-            -z1 * vx1 + z2 * vx2 + x1 * vz1 - x2 * vz2,
-        ],
+        // eq2'': x0 * (vy1-vy3) + y0 * (vx3-vx1) + vx0 * (y3-y1) + vy0 * (x1-x3) = -y1*vx1 + y3*vx3 + x1*vy1 - x3*vy3
         [
             vy1 - vy3,
-            vx1 - vx3,
+            vx3 - vx1,
             0.0,
             y3 - y1,
             x1 - x3,
             0.0,
             -y1 * vx1 + y3 * vx3 + x1 * vy1 - x3 * vy3,
         ],
+        // eq3'': x0 * (vz1-vz2) + z0 * (vx2-vx1) + vx0 * (z2-z1) + vz0 * (x1-x2) = -z1*vx1 + z2*vx2 + x1*vz1 - x2*vz2
         [
-            vy2 - vy3,
-            vx2 - vx3,
+            vz1 - vz2,
             0.0,
-            y3 - y2,
-            x2 - x3,
+            vx2 - vx1,
+            z2 - z1,
             0.0,
-            -y1 * vx1 + y3 * vx3 + x1 * vy1 - x3 * vy3,
+            x1 - x2,
+            -z1 * vx1 + z2 * vx2 + x1 * vz1 - x2 * vz2,
         ],
+        // eq4'': x0 * (vz1-vz3) + z0 * (vx3-vx1) + vx0 * (z3-z1) + vz0 * (x1-x3) = -z1*vx1 + z3*vx3 + x1*vz1 - x3*vz3
         [
-            vz2 - vz3,
+            vz1 - vz3,
             0.0,
-            vx2 - vx3,
-            z3 - z2,
+            vx3 - vx1,
+            z3 - z1,
             0.0,
-            x2 - x3,
-            -z2 * vx2 + z3 * vx3 + x2 * vz2 - x3 * vz3,
+            x1 - x3,
+            -z1 * vx1 + z3 * vx3 + x1 * vz1 - x3 * vz3,
+        ],
+        // eq5'': y0 * (vz1-vz2) + z0 * (vy2-vy1) + vy0 * (z2-z1) + vz0 * (y1-y2) = -z1*vy1 + z2*vy2 + y1*vz1 - y2*vz2
+        [
+            0.0,
+            vz1 - vz2,
+            vy2 - vy1,
+            0.0,
+            z2 - z1,
+            y1 - y2,
+            -z1 * vy1 + z2 * vy2 + y1 * vz1 - y2 * vz2,
+        ],
+        // eq6'': y0 * (vz1-vz3) + z0 * (vy3-vy1) + vy0 * (z3-z1) + vz0 * (y1-y3) = -z1*vy1 + z3*vy3 + y1*vz1 - y3*vz3
+        [
+            0.0,
+            vz1 - vz3,
+            vy3 - vy1,
+            0.0,
+            z3 - z1,
+            y1 - y3,
+            -z1 * vy1 + z3 * vy3 + y1 * vz1 - y3 * vz3,
         ],
     ];
 
@@ -224,9 +233,30 @@ fn solve_stage2(input: &[Hailstone]) -> u64 {
             }
         }
     }
-    dbg!(matrix);
 
-    0
+    let vz0 = matrix[5][6] / matrix[5][5];
+    let vy0 = (matrix[4][6] - matrix[4][5] * vz0) / matrix[4][4];
+    let vx0 = (matrix[3][6] - matrix[3][4] * vy0 - matrix[3][5] * vz0) / matrix[3][3];
+    let z0 = (matrix[2][6] - matrix[2][3] * vx0 - matrix[2][4] * vy0 - matrix[2][5] * vz0)
+        / matrix[2][2];
+    let y0 = (matrix[1][6]
+        - matrix[1][2] * z0
+        - matrix[1][3] * vx0
+        - matrix[1][4] * vy0
+        - matrix[1][5] * vz0)
+        / matrix[1][1];
+    let x0 = (matrix[0][6]
+        - matrix[0][1] * y0
+        - matrix[0][2] * z0
+        - matrix[0][3] * vx0
+        - matrix[0][4] * vy0
+        - matrix[0][5] * vz0)
+        / matrix[0][0];
+
+    let x0 = x0.round() as i64;
+    let y0 = y0.round() as i64;
+    let z0 = z0.round() as i64;
+    x0 + y0 + z0
 }
 
 pub struct Day24Solver;
@@ -235,7 +265,7 @@ impl AdventOfCodeDay<'_> for Day24Solver {
 
     type Part1Output = u64;
 
-    type Part2Output = u64;
+    type Part2Output = i64;
 
     fn solve_part1(input: &Self::ParsedInput) -> Self::Part1Output {
         solve_stage1(input, (200000000000000, 400000000000000))
@@ -269,6 +299,6 @@ mod tests {
     #[test]
     fn test_stage2() {
         let input = Day24Solver::parse_input(TEST_INPUT);
-        assert_eq!(super::solve_stage2(&input), 154);
+        assert_eq!(super::solve_stage2(&input), 47);
     }
 }
